46aaf482716b404dfdf096d1090a58a475feb2cd
zntb <zenetibi@yahoo.com>
2024-12-06 15:57:51 +0200

    Add Reminder Email

---
+++ app/api/email/[invoiceId]/route.ts
@@ -0,0 +1,61 @@
+import prisma from '@/app/utils/db';
+import { requireUser } from '@/app/utils/hooks';
+import { emailClient } from '@/app/utils/mailtrap';
+import { NextResponse } from 'next/server';
+
+export async function POST(
+  request: Request,
+  {
+    params,
+  }: {
+    params: Promise<{ invoiceId: string }>;
+  },
+) {
+  try {
+    const session = await requireUser();
+
+    const { invoiceId } = await params;
+
+    const invoiceData = await prisma.invoice.findUnique({
+      where: {
+        id: invoiceId,
+        userId: session.user?.id,
+      },
+    });
+
+    if (!invoiceData) {
+      return NextResponse.json(
+        { error: 'Invoice not found' },
+        { status: 404 },
+      );
+    }
+
+    const sender = {
+      email: 'hello@demomailtrap.com',
+      name: 'Zntb',
+    };
+
+    emailClient.send({
+      from: sender,
+      to: [{ email: 'zenetibi@yahoo.com' }],
+      template_uuid: '38ab56c6-76d3-4c0f-a2a0-bb64eb6f5999',
+      template_variables: {
+        first_name: invoiceData.clientName,
+        company_info_name: 'InvoiceZntb',
+        company_info_address: 'Chad street 124',
+        company_info_city: 'Dublin',
+        company_info_zip_code: '1234',
+        company_info_country: 'Ireland',
+      },
+    });
+
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      { error: 'Failed to send Email reminder' },
+      { status: 500 },
+    );
+  }
+}
